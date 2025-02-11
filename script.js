6c0ab62630cfacf31d3622d2fd59863341fe537a
xXaNdreixX12 <49099080+xXaNdreixX12@users.noreply.github.com>
2025-02-11 19:27:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = 'esti sigura?..'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
